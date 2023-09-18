54e8d628bf9c17022cd65dd26671fdb1c8ede4d8
sanjaych0311 <sanjaych0311@gmail.com>
2023-09-18 18:57:39 +0530

    Initial development commit

---
+++ webapp-dev/scripts/script.js
@@ -0,0 +1,9 @@
+// scripts/script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const clickMeButton = document.getElementById("clickMeButton");
+    clickMeButton.addEventListener("click", function () {
+        alert("Button clicked!");
+    });
+});
+
